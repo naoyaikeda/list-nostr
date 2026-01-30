3bc53a38051fb617f339425ea626f50261914a3e
Naoya Ikeda <naoya.ikeda@exchange.grayrecord.com>
2026-01-30 11:37:19 +0900

    Rich追加

--- list-nostr.py
+++ list-nostr.py
@@ -3,6 +3,8 @@ import uuid
 import argparse
 import yaml
 import os
+import rich
+from rich.console import Console
 from dotenv import load_dotenv
 from pynostr.relay_manager import RelayManager
 from pynostr.filters import FiltersList, Filters
@@ -17,19 +19,21 @@ def load_config(profile_name: str):
     profiles_path = os.path.join(home_dir, "post-nostr-profiles.yaml")
     with open(profiles_path, 'r') as file:
         profiles = yaml.safe_load(file)
-    
+
     common_relays = profiles['common']['relays']
     selected_profile = profiles['profiles'].get(profile_name)
 
     relays = set(common_relays)
     for relay in selected_profile.get('relays', []):
         relays.add(relay)
-    
+
     active_profile = {'nsec': selected_profile['nsec'], 'relays': list(relays)}
 
     return active_profile
 
 def main():
+    console = Console()
+
     parser = argparse.ArgumentParser(description="list nostr chats")
     parser.add_argument("-p", "--profile", type=str, default="default", help="Profile name to use")
     parser.add_argument("-f", "--from", dest="source_scope", choices=["public", "follow"], default="public", help="Source of events: public (default) or follow")
@@ -38,14 +42,13 @@ def main():
     profile_name = args.profile
     active_profile = load_config(profile_name)
 
-    print(f"Active profile: {profile_name}")
-    print(active_profile)
+    console.print(f"Active profile: {profile_name}")
 
     relay_manager = RelayManager(timeout=2)
 
     for relay in active_profile['relays']:
         relay_manager.add_relay(relay)
-    
+
     filters_list = []
 
     if args.source_scope == "follow":
@@ -56,7 +59,7 @@ def main():
             private_key = PrivateKey.from_hex(nsec)
 
         pubkey_hex = private_key.public_key.hex()
-        print(f"Fetching contacts for {pubkey_hex}...")
+        console.print(f"Fetching contacts for {pubkey_hex}...")
 
         # Fetch contacts
         contact_sub_id = uuid.uuid1().hex
@@ -80,7 +83,7 @@ def main():
 
         authors = follows + [pubkey_hex]
         filters_list = [Filters(kinds=[EventKind.TEXT_NOTE], authors=authors, limit=100)]
-        print(f"Fetching timeline for {len(follows)} follows...")
+        console.print(f"Fetching timeline for {len(follows)} follows...")
     else:
         filters_list = [Filters(kinds=[EventKind.TEXT_NOTE], limit=100)]
 
@@ -91,10 +94,10 @@ def main():
     relay_manager.run_sync()
     while relay_manager.message_pool.has_notices():
         notice_msg = relay_manager.message_pool.get_notice()
-        print(notice_msg.content)
+        console.print(notice_msg.content)
     while relay_manager.message_pool.has_events():
         event_msg = relay_manager.message_pool.get_event()
-        print(event_msg.event.content)
+        console.print(event_msg.event.content)
 
     relay_manager.close_all_relay_connections()
 
